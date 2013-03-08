9b7be7e8520ae251a226c07f87980fbc9f5e6570
Kazuyoshi Goto <aimluck.goto@gmail.com>
2013-03-08 21:37:07 +0900

    update

--- theme-01/htdocs/js/scripts.js
+++ theme-01/htdocs/js/scripts.js
@@ -6,3 +6,17 @@ $(window).resize(function(){
 	var bh = $(window).height() - 80;
 	$("#content").css("min-height",bh);
 });
+
+//Bootstrap Plugins
+$(function () {
+	//tooltip
+	$('.ttip').tooltip();
+	$('.ttipBottom').tooltip({placement:'bottom'});
+	$('.ttipRight').tooltip({placement:'right'});
+	$('.ttipLeft').tooltip({placement:'left'});
+	$('.ttipFocus').tooltip({trigger:'focus'});
+
+	//alert
+	$(".alert").alert();
+
+});
